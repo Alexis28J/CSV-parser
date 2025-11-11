b016cf032eadc0db7dea78240d242b1d39a2f258
Alexis28J <jordy2806@hotmail.com>
2025-11-11 09:22:53 +0100

    Piccolo ripasso (11.11.2025)

--- script.js
+++ script.js
@@ -19,27 +19,30 @@
 
 //6) Ogni giro creo un oggetto vuoto che deve contenere uno studente
 
-//7) Spezzo le proprietà e i valori in due array di stringhe (li spezzo nei punti e virgola)
+//7) Spezzo le proprietà e i valori in due array di stringhe (li spezzo nei punti e virgola), ogni riga una stringa
 
-//8) Faccio un ciclo interno e su l'array di proprietà e metto dentro lo studente il valore corrispondente alla proprietà (proprietà e valore)
+//8) Faccio un ciclo interno e sull'array delle proprietà e metto dentro lo studente il valore corrispondente alla proprietà (proprietà e valore)
 
-//9) Scrivo il risultato su file - fuori dal ciclo interno metto lo studente creato nell'array degli studenti
+//9) Fuori dal ciclo interno metto lo studente creato nell'array degli studenti
+
+//10) Converto l'Array creato in stringa
+
+//11) Scrivo il risultato sul file
 //https://nodejs.org/en/learn/manipulating-files/writing-files-with-nodejs (Come scrivere un risultato su un file)
-
-
 
 
 const fs = require('node:fs');  //1) primo task      //"require" sta per "richiesto"
 
 let data;
 
+//blocco "try/catch" - serve a prevenire che il codice si rompa
 try {
     data = fs.readFileSync('Input/students.csv', 'utf8'); //windows usa lo reverse backslash (\) ma per usarlo in Javascript dobbiamo cambiarla
     // console.log(data);
-} catch (err) {
+} catch (err) {  // in caso il codice si rompa mi verrà un "catch"
     console.error(err);
 }
-const stringArray = data.split(/\r?\n/);  //2) secondo task  
+const stringArray = data.split(/\r?\n/);  //2) secondo task  //per noi basterebbe mettere solo\n
 //la differenza tra \r e \n è che sul tuo sistema operativo, uno di questi è il carattere corretto con cui terminare una riga, e l'altro no
 //Windows considera \r\n insieme come il carattere corretto per la fine della riga.
 const propertiesString = stringArray.shift();  //3) terzo task
@@ -67,13 +70,13 @@ for (let i = 0; i < stringArray.length; i++) {  //5) quinto task
     // console.log("------------------------------------------------------------");
 
     const propertiesArray = propertiesString.split(";"); //7) settimo task
-    const valuesArray = valueString.split(";");
+    const valuesArray = valueString.split(";"); //invece di mettere a capo \n, metto ; che è la fine della mia stringa
 
     // console.log(propertiesArray);    
     // console.log(valuesArray);
     
     
-    for (let j = 0; j < propertiesArray.length; j++) {  //ottavo task
+    for (let j = 0; j < propertiesArray.length; j++) {  //8) ottavo task
         const property = propertiesArray[j];
         const value = valuesArray[j];
         student[property] = value;
@@ -86,10 +89,10 @@ studentsArray.push(student);     //9) nonno task
 
 console.log(studentsArray);    
 
-const jsonArray = JSON.stringify(studentsArray);  //converto l'array in json
+const jsonString = JSON.stringify(studentsArray); //10) decimo task   //converto l'array in json
 
-try {
-  fs.writeFileSync('./Output/students-data.json', jsonArray);  //salvo il file in formato .json nella cartella Output;  jsonArray è il contenuto del file
+try {  //11) undicesimo task
+  fs.writeFileSync('./Output/students-data.json', jsonString);  //salvo il file in formato .json nella cartella Output;  jsonArray è il contenuto del file
   // file written successfully
 } catch (err) {
   console.error(err);
